367407ebb3f4a39a763076ce3d8e8d0d502e84eb
Muhammad Ishaq | MI21 <mi21.muhammadishaq@gmail.com>
2025-10-19 08:56:22 +0500

    upgrade

--- src/helpers.js
+++ src/helpers.js
@@ -28,7 +28,7 @@ export function getBatsmen() {
 export function getAllRounders() {
     const AllRounders={};
     for (const player in pct_Players){
-        if (pct_Players[player].role === "AllRounder") {
+        if (pct_Players[player].role === "All-rounder") {
             AllRounders[player]=pct_Players[player];
 
         }
@@ -45,6 +45,53 @@ export function getWicketKeepers() {
     return WicketKeepers;
 }
 
+export function getBestFielder() {
+    let bestFielder={
+        totalCatches:0
+    };
+    for (const player in pct_Players){
+        if(bestFielder.totalCatches<pct_Players[player].totalCatches && pct_Players[player].role!=="Wicketkeeper-Batsman"){
+            bestFielder=pct_Players[player];
+        }
+    }
+    return bestFielder;
+}
+
+export function getBestWicketKeeper() {
+    let bestWicketKeeper={
+        totalCatches:0
+    };
+    for (const player in pct_Players){
+        if(bestWicketKeeper.totalCatches<pct_Players[player].totalCatches && pct_Players[player].role==="Wicketkeeper-Batsman"){
+            bestWicketKeeper=pct_Players[player];
+        }
+    }
+    return bestWicketKeeper;
+}
+
+export function getfemalePlayers(){
+    const femalePlayers={};
+    for (const player in pct_Players){
+        if (pct_Players[player].gender==="female") {
+            femalePlayers[player]=pct_Players[player];
+        }
+    }
+    return femalePlayers;
+}
+
+export function getPslTeamsByName(name){
+    const teamsData = pct_Players[name].PSLTeams;
+    if (teamsData) {
+        return teamsData;
+    } else {
+        return { error: "Player not found" };
+    }
+}
+
+export function getPlayerStatesByName(name){
+    const playerData = pct_Players[name]
+}
+
 export function getMoreWicketTaker(){
     let mostWicketTaker={
         totalWickets:0
@@ -55,6 +102,28 @@ export function getMoreWicketTaker(){
         }
     }
     return mostWicketTaker;
+}
+
+export function getMoreRunScorer(){
+   let MoreRunScorer={
+        totalScore:0
+    };
+    for (const player in pct_Players){
+        if(MoreRunScorer.totalScore<pct_Players[player].totalScore){
+            MoreRunScorer=pct_Players[player];
+        }
+    }
+   return MoreRunScorer;
+}
+
+export function getCaptains(){
+    const Captains={};
+    for (const player in pct_Players){
+        if (pct_Players[player].captain===true){
+            Captains[player]=pct_Players[player];
+        }
+    }
+    return Captains;
 }
 
 export function getPlayerByName(name) {
--- src/index.js
+++ src/index.js
@@ -3,37 +3,68 @@ import express from 'express';
 
 const app = express();
 
-app.get('/:player',(req,res)=>{
-    const playerName=req.params.player;
-    res.send(help.getPlayerByName(playerName));
-
-});
 app.get("/" ,(req,res)=>{
     res.send("Welcome to Pakistan Cricket Team Info Server");
 });
 
 app.get("/players", (req, res) => {
-    res.json(help.getPlayers());
+    res.send(help.getPlayers());
 });
 
 app.get("/bowlers", (req, res) => {
-    res.json(help.getBowlers());
+    res.send(help.getBowlers());
 });
 
 app.get("/batsmen", (req, res) => {
-    res.json(help.getBatsmen());
+    res.send(help.getBatsmen());
 });
 
-app.get("/allrounders", (req, res) => {
-    res.json(help.getAllRounders());
+app.get("/all-rounders", (req, res) => {
+    res.send(help.getAllRounders());
 });
 
-app.get("/wicketkeepers", (req, res) => {
-    res.json(help.getWicketKeepers());
+app.get("/wicket-keepers", (req, res) => {
+    res.send(help.getWicketKeepers());
 });
 
-app.get("/mostwickettaker", (req, res) => {
-    res.json(help.getMoreWicketTaker());
+app.get("/most-wicket-taker", (req, res) => {
+    res.send(help.getMoreWicketTaker());
+});
+
+app.get("/most-run-scorer", (req, res) => {
+    res.send(help.getMoreRunScorer());
+});
+
+app.get('/captains', (req,res)=>{
+    res.send(help.getCaptains());
+});
+
+app.get('/best-fielder', (req,res)=>{
+    res.send(help.getBestFielder());
+})
+
+app.get('/best-wicket-keeper', (req,res)=>{
+    res.send(help.getBestWicketKeeper());
+});
+
+app.get("/females" ,(req,res)=>{
+    res.send(help.getfemalePlayers());
+});
+
+app.get('/:player',(req,res)=>{
+    const playerName=req.params.player;
+    res.send(help.getPlayerByName(playerName));
+
+}); 
+
+app.get("/:player/psl-teams",(req,res)=>{
+    const playerName=req.params.player;
+    res.send(help.getPslTeamsByName(playerName));
+});
+
+app.get("/:player/stats",(req,res)=>{
+    const playerName=req.params.player;
+    res.send(help.getPlayerStatesByName(playerName));
 });
 
 app.listen(3000, () => {
